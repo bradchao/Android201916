26b810423bb5c6ff502d49d36d92e6387ef2eb98
brad <brad@brad.tw>
2019-12-18 20:23:40 +0800

    2

--- app/src/main/java/tw/org/iii/android201916/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201916/MainActivity.java
@@ -2,9 +2,25 @@ package tw.org.iii.android201916;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 
 public class MainActivity extends AppCompatActivity {
+    private MyService myService;
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder binder) {
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
